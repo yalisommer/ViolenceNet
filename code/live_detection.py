50957aec5f0c620a8f58eff3a2e78716c85c2179
Daniel Schiffman <daniel_schiffman@brown.edu>
2025-04-29 10:57:10 -0400

    saving content moderation and stuff

--- code/live_detection.py
+++ code/live_detection.py
@@ -6,9 +6,20 @@ from keras.layers import \
        Conv2D, MaxPool2D, Dropout, Flatten, Dense
 from preprocess import Datasets
 from skimage.transform import resize
+# import argparse
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--video_src", type=str, required=True, help="Path to video or live feed to be processed")
+# args = parser.parse_args()
+
+# video_path = args.video_src
+
+# LIVE = False
+# if args.video_src.isdigit():
+#     video_path = int(args.video_src)
+#     LIVE = True
 
 #change this to actual model
-
 model = Sequential([
               ## Add layers here separated by commas.
 
@@ -36,8 +47,10 @@ def preprocess_frame(img):
     img = np.array(img, dtype=np.float32)
     return img
 
-#live video feed
+#video feed
 live_feed = cv2.VideoCapture(0)
+
+# if not LIVE:
 
 while live_feed.isOpened():
     return_val, frame_im = live_feed.read()
@@ -45,13 +58,14 @@ while live_feed.isOpened():
         break
 
     display_frame = frame_im.copy()
+    
     frame_im = preprocess_frame(frame_im)
     frame_im = np.expand_dims(frame_im, axis=0)  # add batch dimension
     prediction = model.predict(frame_im)
 
     #Unsure what this will output it is a tensorflow method
     prediction = model.predict(frame_im)
-    violence_prob = prediction[0][0]
+    violence_prob = prediction[0][1]
     print(violence_prob)
 
     color = (0, 0, 0)
@@ -62,7 +76,6 @@ while live_feed.isOpened():
         label = ""
 
     cv2.putText(display_frame, label, (400, 800), cv2.FONT_HERSHEY_PLAIN, 8, color, 4)
-
     cv2.imshow("Live Detection", display_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -70,9 +83,6 @@ while live_feed.isOpened():
 
 live_feed.release()
 cv2.destroyAllWindows()
-
-
-
 
 
     
