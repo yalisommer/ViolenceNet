105719cf46263ec87394f27dda7ce30765a3fb00
Yali <144831354+yalisommer@users.noreply.github.com>
2025-04-28 20:45:46 -0400

    meow video

--- code/live_detection.py
+++ code/live_detection.py
@@ -1,20 +1,39 @@
 import cv2
 import numpy as np
 import tensorflow as tf
-
+from tensorflow.keras import Sequential
+from keras.layers import \
+       Conv2D, MaxPool2D, Dropout, Flatten, Dense
 from preprocess import Datasets
+from skimage.transform import resize
 
 #change this to actual model
-model = tf.keras.models.load_model() #insert .h5 file in parens
+
+model = Sequential([
+              ## Add layers here separated by commas.
+
+              #idea: have two conv before pool, change size to be smaller than 7
+              Conv2D(15, kernel_size=7, strides=1, activation='relu'), # 10 conv kernals each size 5x5 with stride 1
+              MaxPool2D(2), # should I be naming these? 2 here means that the size of the downscaled pool is 2x2
+              Dense(64, activation='relu'), # was 32
+              Dropout(0.5),
+              Flatten(),
+              Dense(2, activation='softmax')
+              # probably overfitting -> 2 dense and one conv
+        ])
+model.build()
+model.load_weights("/Users/yalisommer/Desktop/Schoolwork/CS/CS1430/ViolenceNet-Darpli/code/checkpoints/your_model/042825-151251/your.e048-acc0.8863.weights.h5") #insert .h5 file in parens
 
 #unsure what to put for path (but I know it should be like the data directory 
 # bc the preprocess Datasets code uses it that way I'm j not sure if its formatted corretcly)
-datasets = Datasets("/data", 1)
+datasets = Datasets("/Users/yalisommer/Desktop/Schoolwork/CS/CS1430/ViolenceNet-Darpli/data", 1)
 
 #helper method to preprocess images in the same way we did for training
 def preprocess_frame(img):
+    img = resize(img, (224, 224, 3), preserve_range=True)
     img = img / 255
     img = datasets.standardize(img)
+    img = np.array(img, dtype=np.float32)
     return img
 
 #live video feed
@@ -25,23 +44,28 @@ while live_feed.isOpened():
     if not return_val:
         break
 
+    display_frame = frame_im.copy()
     frame_im = preprocess_frame(frame_im)
+    frame_im = np.expand_dims(frame_im, axis=0)  # add batch dimension
+    prediction = model.predict(frame_im)
 
     #Unsure what this will output it is a tensorflow method
-    predictions = model.predict(frame_im)
-    print("this is the prediction of the model:" + predictions)
+    prediction = model.predict(frame_im)
+    violence_prob = prediction[0][0]
+    print(violence_prob)
 
-    #assign label based on prediction
-    label = 
+    color = (0, 0, 0)
+    if violence_prob > 0.55:
+        label = "VIOLENCE DETECTED"
+        color = (0, 0, 255)
+    else:
+        label = ""
 
-    #assign color based on prediction
-    color = 
+    cv2.putText(display_frame, label, (400, 800), cv2.FONT_HERSHEY_PLAIN, 8, color, 4)
 
-    cv2.putText(frame_im, f"{label}", cv2.FONT_HERSHEY_PLAIN, 1, color)
+    cv2.imshow("Live Detection", display_frame)
 
-    cv2.imshow("Live Detection", frame_im)
-
-    if cv2.waitKey(1) & 0xFF == ord('e'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 live_feed.release()
