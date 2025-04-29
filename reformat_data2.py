6505403b74624ab17bd453b2977a73dfce5117cf
Yali <144831354+yalisommer@users.noreply.github.com>
2025-04-29 12:19:23 -0400

    testetetet

---
+++ reformat_data2.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# Split data into V / NV format instead of based on csv
+move_dirs = ['test', 'train', 'valid']
+
+for dir in move_dirs:
+    source_folder = f'data/{dir}'
+    v_folder = f'data/{dir}/V'
+    nv_folder = f'data/{dir}/NV'
+
+    os.makedirs(v_folder, exist_ok=True)
+    os.makedirs(nv_folder, exist_ok=True)
+
+
+    for filename in os.listdir(source_folder):
+        if filename.lower().endswith(('.jpg')):
+            first_word = filename.split('_')[0]
+            src_path = os.path.join(source_folder, filename)
+
+            if first_word == 'V':
+                dest_path = os.path.join(v_folder, filename)
+            elif first_word == 'NV':
+                dest_path = os.path.join(nv_folder, filename)
+            else:
+                continue
+
+            shutil.move(src_path, dest_path)
